3d1c1cfc13d8ad6ee7ff3bea2ee1eb9c52f45be9
Kayo Araujo <kayoaruj@gmail.com>
2026-06-26 20:49:40 -0300

    fix: atualização da classe Figura para herdar de ABC e adição do método abstrato desenhar

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter import colorchooser
 from tkinter import ttk
+from abc import ABC, abstractmethod
 
 # Criação da classe Figura
-class Figura():
+class Figura(ABC):
     def __init__(self, values, cor_borda, cor_preenchimento):
         self.values = values
         self.cor_borda = cor_borda
         self.cor_preenchimento = cor_preenchimento
-    
+
+    @abstractmethod    
     def desenhar(self, canvas):
         pass
 
